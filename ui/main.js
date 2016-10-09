7f6eb3704d84f51a9f008c4d08a16046ca109b4f
sattybhatt <mailmesatyambhatt@gmail.com>
2016-10-09 11:49:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var cnt=document.getElementById('click');
+var counter=0;
+cnt.onclick(function(){
+    counter=counter+1;
+   var cnt2=document.getElementById('cnt');
+   cnt2.innerHTML(cnt2);
+});
