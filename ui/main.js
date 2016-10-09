d01ce501a2ff45c8847d00e2cea3b1cd87ad9e44
sattybhatt <mailmesatyambhatt@gmail.com>
2016-10-09 12:04:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var cnter=document.getElementById('click');
 var counter=0;
 cnter.onclick=function(){
-    console.log(counter)
+    console.log(counter);
     counter=counter+1;
    var cnt2=document.getElementsByClassName('cnt');
    cnt2.innerHTML=counter.toString();
