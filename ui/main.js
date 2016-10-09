5854d93a8abdc2df59cca3dee5489c83197a6299
sattybhatt <mailmesatyambhatt@gmail.com>
2016-10-09 13:55:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
 var cnter=document.getElementById('click');
 var counter=0;
-cnter.onclick=function(){
+var request=new XMLHttpRequest();
+request.onreadystatechange=function()
+{
+  
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+         if(request.status==200)
+         {
+          var counter=request.responsetext();
+          var cnt2=document.getElementsByClassName('cnt');
+         cnt2[0].innerHTML=counter.toString();
+         }
+    }
+    request.open('GET','http://sattybhatt.imad.hasura-app.io/counter',true);
+};
+/*cnter.onclick=function(){
     console.log(counter);
     counter=counter+1;
    var cnt2=document.getElementsByClassName('cnt');
    cnt2[0].innerHTML=counter.toString();
-};
+}; */
