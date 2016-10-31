f23ba9b2f5d51dac32010d77856e5e813963b061
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-01 00:53:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,29 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
+//db connection
+var Pool = require('pg').Pool;
+var config = {
+    user: 'sattybhatt',
+    database:'sattybhatt',
+    host:'http://db.imad.hasura-app.io/',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query("SELECT * from test",function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+         res.send(JSON.stringify(result));  
+       }
+    });
+});
+
+
+//end
 var ob={
     title:"Article-one",
     heading:"Article-one",
