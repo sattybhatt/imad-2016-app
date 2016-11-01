139860d9454ae2fc9c759364e1441265ba770903
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-01 13:08:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/articles/:articleName',function(req,res){
          res.send(hello(articleData));
        }
    } */
-   });
+   })
 });
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
