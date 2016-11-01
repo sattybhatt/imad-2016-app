de71e6215d32b0abef4172fe306aafcc4f62d565
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-01 12:12:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,7 @@ app.get('/ui/main.js',function(req,res){
 app.get('/articles/:articleName',function(req,res){
    var articleName=req.params.articleName;
    pool.query("SELECT * from article WHERE title="+articleName,function(err,res){
+       console.log(res);
    if(err){
        res.status(500).send(err.toString());
    }
