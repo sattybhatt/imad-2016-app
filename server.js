5f4d0905db524beb443ddd629e994587bc936d81
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-10 23:46:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,56 @@ app.get('/articles/:articleName',function(req,res){
    } 
    });
 }); 
+
+//twice attempt
+app.get('/blog1',function(req,res){
+   pool.query("SELECT * from article ORDER by time DESC",function(err,result){
+   if(err){
+       res.status(500).send(err.toString());
+   } 
+   else{
+       if(result.rows.length===0){
+         res.status(400).send('No blog found');  
+       }else{
+        res.send(makepage(result.rows));
+       }
+   } 
+   });
+}); 
+//end here
+//function makepage
+function makepage(ob1)
+ {
+    for(var m=0;m<ob1.length;m++){
+	var title=ob1[m].title;
+	var img=ob1[m].image;
+	
+var htmltemplate= '<html><head><title>Blogs</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body>';
+	htmltemplate +='<div style="display: inline-block;">';
+if(m===0){	
+	htmltemplate +='<div class="half half1" style="background:url('+img+')"><div class="gradient"></div><span class="cover-title"><a class="nw" href="blog/'+title+'">'+title+'</a></h2></div>';
+	}
+	if(m==1){ 
+	htmltemplate +='<div class="half half2" style="background:url('+img+')"><span class="cover-title2"><a class="nw" href="blog/'+title+'">'+title+'</a></h2></div>';
+	}
+	if(m==2){ 
+	htmltemplate +='<div class="half half3" style="background:url('+img+')"><span class="cover-title2"><a class="nw" href="blog/'+title+'">'+title+'</a></h2></div>';
+	}
+	if(m==3){	
+	htmltemplate +='<div class="half half4" style="background:url('+img+')"><span class="cover-title2"><a class="nw" href="blog/'+title+'">'+title+'</a></h2></div>';
+	}
+	htmltemplate +='</div><div class="below" style="margin-top:350px;"><span class="news">Latest news</span><ul>';
+	if(m>=4 &&m <=7){
+	htmltemplate +='<li class="two"><div class="story" style="width:270px;height:221px;"><a href="#"><img src='+img+' width="270px" height="135px"></a>';
+	htmltemplate +='<h4 style="color:white;font-family: ARS Maquette,Helvetica Neue,Arial,sans-serif;">'+title+'</h4><span class="cre">Bryan clarks-13hrs ago</span></div></li>';}
+	htmltemplate +='</ul></div><div class="river-flow"><ul class="river">';
+	if(m>7){
+	htmltemplate +='<li class="lis"><div class="tags"><a href="#" class="tag">Apps</a></div><div class="block-content" style="margin-top:25px;position:absolute;">';
+	htmltemplate +='<h2 style="color:#F42">'+title+'</h2><span class="image"><a href="#"><img src='+img+' width="200px" height="150px" style="margin-left:820px;position:absolute;"></a></span>Read more</div></li>';
+	htmltemplate +='</div></body></html>'; }
+     return htmltemplate;
+}}
+//end here
 app.get('/article-two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
