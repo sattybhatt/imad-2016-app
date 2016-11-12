2a8da60f65ec88c32f929407df633a125d579c5c
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-12 19:51:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
 var app = express();
+var bodyParser=require('body-parser');
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -45,6 +46,21 @@ app.get('/test-db',function(req,res){
        }
     });
 });
+//create user
+app.post('/create user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query("INSERT into user(username,password) VALUES($1,$2)",[username,dbString],function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+         res.send("Username created:",username);
+       }
+    });
+});
+//end here
 function hello(ob1)
  {
     
