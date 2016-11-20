eefba2cbc077cb084284d84440c30cbbf5ba6a79
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-20 14:36:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/create-user',function(req,res){
        if(err){
            res.status(500).send(err.toString());
        } else{
-         res.send("Username created:",username);
+         alert("Username created:"+username);
        }
     });
 });
