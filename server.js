2cc4a9f570aa7ed8b6bcbbd1c92f837975bb826d
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-20 17:26:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -246,8 +246,8 @@ function makecontent(ob1){
     var image=ob1.image;
     var content=ob1.content;
     var date=ob1.date;
-    var template='<html><head></script><title>'+title+'</title></head><body><div>Date:'+date.toString()+'</div><div><img src='+image+'>';
-    template +='</div><div>'+title+'</div><div>'+content+'</div><input type="text" name="comment" id="comment"><input type="submit" name="submitc" id="submitc" value="submit"><script type="text/javascript" src="/ui/main2.js"></body></html>';
+    var template='<html><head><title>'+title+'</title></head><body><div>Date:'+date.toString()+'</div><div><img src='+image+'>';
+    template +='</div><div>'+title+'</div><div>'+content+'</div><input type="text" name="comment" id="comment"><input type="submit" name="submitc" id="submitc" value="submit"><script type="text/javascript" src="/ui/main2.js"></script></body></html>';
     return template;
 }
 //end here
