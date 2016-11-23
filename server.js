7e83ee8a67b8c08a7d19ed83690f47e8b478aa75
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-24 00:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -243,12 +243,13 @@ app.get('/blog/:blogName',function(req,res){
 //end here
 //function makecontent
 function makecontent(ob1){
-    var title=ob1.title;
+  /*  var title=ob1.title;
     var image=ob1.image;
     var content=ob1.content;
     var date=ob1.date;
 	var aid=ob1.id;
 	var ob2="";
+	var template="";
 	pool.query("SELECT * from comment WHERE aid=$1",[aid],function(err,result){
        if(err){
            res.status(500).send(err.toString());
@@ -281,7 +282,7 @@ function makecontent(ob1){
              })
 	template +='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+username+'</div><div style="margin-top: 7px;">'+ob2[n].comment+'</div></div></div>';}
 	template +='</div></div></div></div></div></div><div class="artmore"></div><div class="footer"></div></div>';
-	return template;
+	return template; */
 }
 //end here
 app.get('/article-two',function(req,res){
