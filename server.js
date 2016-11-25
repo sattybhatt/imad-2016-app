2a55d04ecbd7f793b27e3b86a770279c8f6bd9f4
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-25 16:56:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -240,7 +240,7 @@ app.get('/blog/:blogName',function(req,res){
    });
 }); 
  //window.comm="";
- var comm="";
+ global.comm="";
 function returnc(aid2){
     var aid=aid2;
 		pool.query("SELECT * from comment WHERE aid=$1",[aid],function(err,result){
@@ -264,15 +264,15 @@ function returnc(aid2){
          else{
              var username=result9.rows[0].username.toString();
              console.log(username);
-     comm +='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+username+'</div><div style="margin-top: 7px;">hello</div></div></div>';
+     global.comm +='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+username+'</div><div style="margin-top: 7px;">hello</div></div></div>';
             
              }
              })}
              
              }
              })
-             console.log('comm'+comm);
-      return comm;       
+             console.log('comm'+global.comm);
+      return global.comm;       
 }
 
 //end here
