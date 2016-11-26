2d0b02767c27901eb0d8060ce4d6ceffae7cfdcd
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-26 20:05:46 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,3 +1,16 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var crypto = require('crypto');
+var session=require('express-session');
+var app = express();
+var bodyParser=require('body-parser');
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret:'randomvalue',
+    cookie:{maxAge:1000*60*60*24*30}
+}));
 //
 var xr=document.getElementById('lcarea');
 xr.onclick=function(){
@@ -27,6 +40,10 @@ request.onreadystatechange=function()
     }
 };
 var comment=document.getElementById('comment').innerHTML;
+//temp code
+           var commer ='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+req.session.auth.username2+'</div><div style="margin-top: 7px;">'+comment+'</div></div></div>';
+            var z2=document.getElementById('lcm');
+            z2.innerHTML = z2.innerHTML +commer;
 var aid=document.getElementById('aid').value;
 //console.log(comment);
      request.open('POST','http://sattybhatt.imad.hasura-app.io/comment',true);
