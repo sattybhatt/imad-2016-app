46abcf31a8de6782be3e96d066c3be6fbfac8337
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-26 21:24:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -328,8 +328,8 @@ app.get('/article-two',function(req,res){
 app.get('/ui/blog1.css',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'blog1.css'));
 });
-app.get('/ui/favicon.ico',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+app.get('favicon-32x32.png',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'favicon-32x32.png'));
 });
 app.get('/ui/blog2.css',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'blog2.css'));
