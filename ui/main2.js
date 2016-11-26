2f824d3502024bd8edfbb631b228f8bf775e5240
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-26 18:48:15 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -20,6 +20,7 @@ request.onreadystatechange=function()
             var comm ='<div style="padding: 15px;position: relative;min-height: 50px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">'+req.session.auth.username2+'</div><div style="margin-top: 7px;">'+comment+'</div></div></div>';
             var z2=document.getElementById('lcm').innerHTML;
             var z3=comm+z2;
+            console.log(z3);
             document.getElementById('lcm').innerHTML=z3;
          }
          else if(request.status==403)
