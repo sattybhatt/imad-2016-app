e1b02076a7d76c8e6d40493529459a45dc4dee9b
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-29 22:34:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -109,33 +109,10 @@ console.log(password);
     request.send(JSON.stringify({username:username,password:password}));
 };
 //end here
-//comment
-/*var submit_btn3=document.getElementById('submitc');
-submit_btn3.onclick=function(){
-    console.log('hello submit button is clicked');
-var request=new XMLHttpRequest();
-request.onreadystatechange=function()
-{
-    if(request.readyState==XMLHttpRequest.DONE)
-    {
-         if(request.status==200)
-         {
-             console.log(request.responseText);
-            alert('Commented');
-         }
-         else if(request.status==403)
-         {
-            alert('USername/password is incorrect');
-         }
-         else if(request.status==500)
-         {
-            alert('Something went wrong on server');
-         }
-    }
-};
-var comment=document.getElementById('comment').value;
-//console.log(comment);
-     request.open('POST','http://sattybhatt.imad.hasura-app.io/comment',true);
-     request.setRequestHeader('Content-type','application/json');
-    request.send(JSON.stringify({comment:comment}));
-}; */
+  var lo2=document.getElementById('lk');
+  lo2.onclick=function(){
+     lo2.style.display='none'; 
+      var lc=document.getElementById('mm');
+    lc.style.opacity='1.0';
+  }
+    
