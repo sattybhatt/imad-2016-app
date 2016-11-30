67aaf991a5ba0712c9d02616c6b6bbe160b5e4a8
sattybhatt <mailmesatyambhatt@gmail.com>
2016-11-30 12:50:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -196,15 +196,14 @@ app.get('/',function(req,res){
          res.status(400).send('No blog found');  
        }
        else{
-        var usrd=req.session.auth.userid;
-        res.send(makepage(result.rows,usrd));
+        res.send(makepage(result.rows));
        }
    } 
    });
 }); 
 //end here
 //function makepage
-function makepage(ob1,usrd)
+function makepage(ob1)
  {
     //start new
 	var htmltemplate ='<html><head><link rel="icon" type="image/png" href="/ui/favicon-32x32.png" sizes="32x32" /><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="/ui/blog2.css" rel="stylesheet" /><title>Sattyam Bhatt\'s Blog</title><link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet"></head><body><header><div style="display:inline-block;"><img class="lgo" src="http://www.clker.com/cliparts/4/r/X/K/j/e/quill-feather-pen-md.png" style=""><span style="">Sattyam Bhatt\'s Blog</span></div>';
@@ -214,8 +213,8 @@ function makepage(ob1,usrd)
 	htmltemplate+='<div id="lk" style="display:none;padding: 15px;position: relative;height: 100px;background: rgba(204, 204, 204, 0.07);border-radius: 5px;border: 1px solid #f5f5f5;"><div id="addo" style="width: 240px;margin-left: 40%;margin-top: 200px;padding: 15px;position: absolute;height: 100px;background: white;border-radius: 5px;border: 1px solid #f5f5f5;box-shadow: 1px 1px 6px -3px #7d7d7d;margin-bottom: 20px;z-index: 900;"><div style="position: absolute; height: 50px; width: 50px; border-radius: 50%; top:15px; left:15px; overflow: hidden;"><img src="http://i2.wp.com/www.wallpapersbyte.com/wp-content/uploads/2015/06/Space-Galaxy-Circular-Nebula-Vortex-Stars-In-Dark-Blue-Color-WallpapersByte-com-1366x768.jpg?resize=1024%2C576" style="width: 100%; height: 100%;"></div><div style="width:calc(100% - 50px); margin-left: 65px;"><div style="font-weight: bold; ">Registered</div><div style="margin-top: 7px;">Click here to login</div></div></div></div>';
 htmltemplate+='<div class="mm" id="mm"><div class="mid"><div class="container"><div class="head"></div><div class="arcts">';
 	//check code
-	console.log(usrd);
-	if(usrd){
+//	console.log(usrd);
+	 if(req.session && req.session.auth && req.session.auth.userid){
 	        var n2=document.getElementById('gm');
              var n3=document.getElementById('lgin');
              n3.style.display='none';
