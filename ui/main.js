f101c879f9f0b0e1455383b3d5f0de3be9696e41
sattybhatt <mailmesatyambhatt@gmail.com>
2016-12-01 12:16:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -123,6 +123,8 @@ console.log(password);
       gm[1].style.display='none';
       var lgin=document.getElementById('lgin');
       lgin.style.display='inline';
+      var logout=document.getElementById('logout');
+      logout.style.display='none';
   };
  
     
